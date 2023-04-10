f9b697661b69a8d384e15051fc5cbb7857407742
Tudor Radoni <tudor.radoni@gmail.com>
2023-04-10 23:54:50 +0300

    Add some more problems

---
+++ Other problems/Caesar Cipher/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace Caesar_Cipher
+{
+    internal class Program
+    {
+        public static string StringChallenge(string str, int num)
+        {
+            StringBuilder result = new StringBuilder();
+
+            // Iterate through each character in the string
+            foreach (char c in str)
+            {
+                // Check if the character is an uppercase or lowercase letter
+                if (char.IsLetter(c))
+                {
+                    // Get the ASCII code for the character
+                    int asciiCode = (int)c;
+
+                    // Calculate the shifted ASCII code
+                    int shiftedAsciiCode = asciiCode + num;
+
+                    // Check if the shifted ASCII code is out of range for letters
+                    if ((char.IsLower(c) && shiftedAsciiCode > (int)'z') || (char.IsUpper(c) && shiftedAsciiCode > (int)'Z'))
+                    {
+                        // Wrap around to the beginning of the alphabet
+                        shiftedAsciiCode -= 26;
+                    }
+
+                    // Convert the shifted ASCII code back to a character and append it to the result
+                    char shiftedChar = (char)shiftedAsciiCode;
+                    result.Append(shiftedChar);
+                }
+                else
+                {
+                    // Append non-letter characters to the result as-is
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringChallenge("Hello, World!", 2));
+        }
+    }
+}
---
+++ Other problems/Paranthesis Combinations/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Paranthesis_Combinations
+{
+    internal class Program
+    {
+        public static int CombinatoricsChallenge(int num)
+        {
+            // Edge case: if num is zero, there is only one valid combination (an empty string)
+            if (num == 0)
+            {
+                return 1;
+            }
+
+            // Use a recursive function to generate all possible combinations
+            List<string> generateCombinations(int openCount, int closeCount, string combination)
+            {
+                // Base case: if we have used all the pairs of parentheses, the combination is valid
+                if (openCount == num && closeCount == num)
+                {
+                    return new List<string> { combination };
+                }
+
+                var result = new List<string>();
+
+                // If we have used fewer than num opening parentheses, we can add an opening parenthesis
+                if (openCount < num)
+                {
+                    result.AddRange(generateCombinations(openCount + 1, closeCount, combination + "("));
+                }
+
+                // If we have used fewer closing parentheses than opening parentheses, we can add a closing parenthesis
+                if (closeCount < openCount)
+                {
+                    result.AddRange(generateCombinations(openCount, closeCount + 1, combination + ")"));
+                }
+
+                return result;
+            }
+
+            // Call the recursive function to generate all possible combinations and return the count
+            return generateCombinations(0, 0, "").Count;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CombinatoricsChallenge(3));
+        }
+    }
+}
